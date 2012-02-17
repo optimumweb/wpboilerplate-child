e36440be503cdb98f87463fc3668df69ba4e66b9
Jonathan Roy <jroy@optimumweb.ca>
2012-02-17 18:56:57 -0500

    Add enqueue and same file mixins

---
+++ inc/wpbp-custom.php
@@ -0,0 +1,13 @@
+<?php
+
+// ENQUEUE
+function enqueue() {
+	if ( !is_admin() ) {
+		// scripts
+		wpbp_enqueue_scripts( array( 'modernizr', 'jquery', 'wpbp' ) );
+		// styles
+		wpbp_enqueue_styles( array( '960gs', 'default' ) );
+		wp_enqueue_style('theme', get_stylesheet_directory_uri() . '/css/custom.css', array('default'));
+	}
+}
+add_action('init', 'enqueue');
