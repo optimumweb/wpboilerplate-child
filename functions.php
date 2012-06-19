01786f423cec526a26d8aa5599732c6b25901d86
Jonathan Roy <jroy@optimumweb.ca>
2012-06-19 11:18:17 -0400

    Fix bugs

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function enqueue() {
 		wpbp_enqueue_scripts( array( 'modernizr', 'jquery', 'wpbp' ) );
 		// styles
 		wpbp_enqueue_styles( array( '960gs', 'default' ) );
-		wp_enqueue_style('theme', get_stylesheet_directory_uri() . '/css/theme.css', array('default'));
+		wp_enqueue_style('theme', get_stylesheet_directory_uri() . '/css/master.css', array('default'));
 	}
 }
 add_action('init', 'enqueue');
