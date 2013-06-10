92c1cb667151313b9f6f1a02849dafa782faa959
Jonathan Roy <jroy@optimumweb.ca>
2013-06-10 12:02:43 -0400

    Fix

--- inc/wpbp-custom.php
+++ inc/wpbp-custom.php
@@ -8,7 +8,7 @@ function enqueue() {
 		// scripts
 		wp_enqueue_script('theme', THEME_URI . '/js/scripts.js', array('jquery'));
 		// styles
-		wp_enqueue_style('theme', THEME_URI . '/css/master.css', array('default'));
+		wp_enqueue_style('theme', THEME_URI . '/css/master.css', array('wpbp'));
 	}
 }
 add_action('init', 'enqueue');
