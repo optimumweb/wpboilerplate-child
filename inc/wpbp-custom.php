22927c5ff4a690bbfb5e744570fca3975832e547
Jonathan Roy <jroy@optimumweb.ca>
2020-11-28 19:01:14 -0500

    respect WordPress coding style

--- inc/wpbp-custom.php
+++ inc/wpbp-custom.php
@@ -1,14 +1,15 @@
 <?php
 
 // ENQUEUE
-function enqueue() {
+function enqueue()
+{
 	if ( !is_admin() ) {
 	    // libs
-	    wpbp_enqueue_lib(array('modernizr', 'jquery', 'wpbp'));
+	    wpbp_enqueue_lib( array( 'modernizr', 'jquery', 'wpbp' ) );
 		// scripts
-		wp_enqueue_script('theme', THEME_URI . '/js/scripts.js', array('jquery'));
+		wp_enqueue_script( 'theme', THEME_URI . '/js/scripts.js', array( 'jquery' ) );
 		// styles
-		wp_enqueue_style('theme', THEME_URI . '/css/master.css', array('wpbp'));
+		wp_enqueue_style( 'theme', THEME_URI . '/css/master.css', array( 'wpbp' ) );
 	}
 }
-add_action('init', 'enqueue');
+add_action( 'init', 'enqueue' );
